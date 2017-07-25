1eb4dfdc17d883b7797526b391c92175e4540b83
Administrator <shiwei.bsw@gmail.com>
2017-07-25 10:56:34 +0800

    complete logging

--- app/src/main/java/com/example/kd/anko/LoggingActivity.kt
+++ app/src/main/java/com/example/kd/anko/LoggingActivity.kt
@@ -2,11 +2,18 @@ package com.example.kd.anko
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_logging.*
+import org.jetbrains.anko.*
 
-class LoggingActivity : AppCompatActivity() {
+class LoggingActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logging)
+        verbose.setOnClickListener { verbose { "I'm verbose logger" } }
+        debug.setOnClickListener { debug { "I'm debug logger" } }
+        info.setOnClickListener { info { "I'm info logger" } }
+        warn.setOnClickListener { warn { "I'm warn logger" } }
+        error.setOnClickListener { error { "I'm error logger" } }
     }
 }
